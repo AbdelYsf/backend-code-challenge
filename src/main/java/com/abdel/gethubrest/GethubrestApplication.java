d013b2a163b76a6efd8a2a99c7b675498feb02d7
abdellah <abdel.ysf@gmail.com>
2020-02-07 20:51:20 +0100

    Initial commit

---
+++ src/main/java/com/abdel/gethubrest/GethubrestApplication.java
@@ -0,0 +1,13 @@
+package com.abdel.gethubrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GethubrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GethubrestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abdel/gethubrest/GethubrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdel.gethubrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GethubrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
